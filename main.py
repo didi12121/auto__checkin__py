0977ac7e301d91296610254a076b45574bcb9129
didi12121 <1968967961@qq.com>
2022-09-20 14:57:58 +0800

    dev分支进行测试扫码登录，可在linux上运行

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import datetime
+import os
 
-from didi import CookieUtil, Checkin
+from didi import CookieUtil, Checkin, Auto
 from apscheduler.schedulers.blocking import BlockingScheduler
+# profilePath = '/root/AutomationProfile'
+profilePath = 'D:\AutomationProfiles'
 
 
 def job():
     print('现在是{},开始执行任务'.format(datetime.datetime.now().strftime('%Y-%m-%d')))
-    cookies = CookieUtil.__get_cookie__by__pro__file(r'user-data-dir=D:\AutomationProfile')
+    cookies = CookieUtil.__get_cookie__by__pro__file(r'user-data-dir=' + profilePath)
     print("获取cookie成功,开始签到")
     pids = [
         '100808445c68f22a32b95efe6c3d025512d13c',
@@ -16,8 +19,18 @@ def job():
         Checkin.to__check__in(cookies=cookies, pid=pid)
 
 
+def init():
+    if not os.path.exists(profilePath):
+        print('====开始初始化=====')
+        Auto.get_cookie(profilePath)
+        print('====初始化结束,开始尝试签到====')
+        job()
+    print('====初始化成功====')
+
+
 if __name__ == '__main__':
-    print('开始执行任务')
+    print('====开始运行脚本====')
+    init()
     scheduler = BlockingScheduler(timezone='Asia/Shanghai')
     scheduler.add_job(job, 'cron', day='*', hour='0', minute='0')
     scheduler.start()
